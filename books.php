fc6e31d4a1070f238b77b928b7c8362451c050d2
TisigheLivinstone <tisighelivinstone1b@gmail.com>
2019-04-29 12:02:41 +0100

    git Comit

---
+++ books.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup Form</title>
+	<link rel="stylesheet" type="text/css" href="Signup.css">
+</head>
+<body>
+	<div class="wrapper">
+		<form>
+			<h2>Signup Here</h2>
+			<input type="text" name="firstname" placeholder="Enter firstname" required="">
+			<input type="text" name="lastname" placeholder="Enter lastname" required="">
+			<input type="text" name="username" placeholder="Enter User Name" required="">
+			<input type="name" name="matriculation_No" placeholder="Enter Matricule" required="">
+			<input type="password" name="phoneNo" placeholder="Enter Phone Number" required="">
+			<input type="text" name="town" placeholder="Enter Your Location(Town)" required="">
+			<input type="email" name="email" placeholder="example@gmail.com" required="">
+			<input type="password" name="password" placeholder="Enter Password" required=""><br>
+			<span style="font-size: 18px;">Date Of Birth</span><br><br>
+
+
+		<select name="Dobday">
+			<option>Day</option>
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+			<option value="9">9</option>
+			<option value="10">10</option>
+			<option value="11">11</option>
+			<option value="12">12</option>
+			<option value="13">13</option>
+			<option value="14">14</option>
+			<option value="15">15</option>
+			<option value="16">16</option>
+			<option value="17">17</option>
+			<option value="20">20</option>
+			<option value="21">21</option>
+			<option value="22">22</option>
+			<option value="23">23</option>
+			<option value="24">24</option>
+			<option value="25">25</option>
+			<option value="26">26</option>
+			<option value="27">27</option>
+			<option value="28">28</option>
+			<option value="29">29</option>
+			<option value="30">30</option>
+			<option value="31">31</option>
+			
+		</select>
+
+		<select name="Dobmonth">
+			<option>Month</option>
+			<option value="january">January</option>
+			<option value="february">February</option>
+			<option value="narch">March</option>
+			<option value="aprily">April</option>
+			<option value="may">May</option>
+			<option value="june">June</option>
+			<option value="july">July</option>
+			<option value="august">August</option>
+			<option value="september">September</option>
+			<option value="october">October</option>
+			<option value="november">November</option>
+			<option value="december">December</option>
+
+		</select>
+
+		<select name="Dobyear">
+			<option>Year</option>
+			<option value="2018">2018</option>
+			<option value="2017">2017</option>
+			<option value="2016">2016</option>
+			<option value="2015">2015</option>
+			<option value="2014">2014</option>
+			<option value="2013">2013</option>
+			<option value="2012">2012</option>
+			<option value="2011">2011</option>
+			<option value="2010">2010</option>
+			<option value="2009">2009</option>
+			<option value="2008">2008</option>
+			<option value="2007">2007</option>
+			<option value="2006">2006</option>
+			<option value="2005">2005</option>
+			<option value="2004">2004</option>
+			<option value="2003">2003</option>
+			<option value="2002">2002</option>
+			<option value="2001">2001</option>
+			<option value="2000">2000</option>
+			<option value="1999">1999</option>
+			<option value="1998">1998</option>
+			<option value="1997">1997</option>
+			<option value="1996">1996</option>
+			<option value="1995">1995</option>
+			<option value="1994">1994</option>
+			<option value="1993">1993</option>
+			<option value="1992">1992</option>
+			<option value="1991">1991</option>
+			<option value="1990">1990</option>
+			<option value="1989">1989</option>
+			<option value="1987">1987</option>
+			<option value="1986">1986</option>
+		</select><br><br>
+		<input type="submit" value="Submit Now">
+	</form>	
+
+	</div>
+
+</body>
+</html>
